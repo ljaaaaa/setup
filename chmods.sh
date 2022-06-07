504e81ca6e5dc650d21f165dd6069fbf964ad219
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-06-07 12:50:08 -0700

    Made shell script to loop through directories and make each .sh and .desktop file executable

---
+++ chmods.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#This script is meant to make all shell scripts and .desktop files executable after getting them from backup USB
+#Place this script in home directory
+
+echo "starting"
+
+chmod +x *.sh; chmod +x *.desktop;
+
+for x in */ ; do
+	echo "$x"
+	cd "$x"
+    	chmod +x *.sh; chmod +x *.desktop
+	cd ..
+echo "end"
+done
