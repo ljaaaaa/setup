16fd9935dae5f4dc94ddf66df25b04abd249428a
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-06-27 15:07:25 -0700

    Added bug to chmods

--- chmods.sh
+++ chmods.sh
@@ -3,6 +3,8 @@
 #This script is meant to make all shell scripts and .desktop files executable after getting them from backup USB
 #Place this script in home directory
 
+#Bug: This program won't loop through directories inside directories
+
 chmod +x ~/*.sh; chmod +x ~/*.desktop;
 
 for x in ~/*/ ; do
