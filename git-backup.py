7ff6c79f02322a2bb71670bae7f32ec6ce2cad11
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-07-06 14:54:25 -0700

    Added python script for github backups

--- git-backup.py
+++ git-backup.py
@@ -1,3 +1,12 @@
 #!/bin/python3
 
-repos = ['game-idea', 'LiljaKiiski', 'swing-template']
+import os
+
+repos = ['setup', 'game-idea', 'LiljaKiiski', 'swing-template', 'pid-sim', 'android-apps', 'gomoku', 'arcade-games', 'robotics-motors', 'robotics-drivetrain', 'robotics-hopper', 'cpp-club-projects', 'client-server-template', 'element-finder', 'incomplete-chess', 'koding-kiiskis-website', 'robotics-leds', 'fun-stuff', 'cpp-projects', 'liljakiiski.github.io']
+
+os.system('mkdir ~/git-backups')
+
+for repo in repos:
+    print('cloning ' + repo + '...')
+    os.system('git clone git@github.com:LiljaKiiski/' + repo)
+    os.system('mv ' + repo + ' ~/git-backups')
