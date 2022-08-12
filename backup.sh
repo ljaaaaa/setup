374d661303ac3860c216efa39759c2a8b934dac5
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-08-12 12:49:04 -0700

    Updated backup.sh

--- backup.sh
+++ backup.sh
@@ -2,7 +2,14 @@
 
 #Script to backup important directories
 
+echo "Creating temp dir"
 mkdir /media/lilja/crabstick/LILJA_BCK/computer_files/temp
+
+echo "Moving previous directories to temp"
 mv /media/lilja/crabstick/LILJA_BCK/computer_files/* /media/lilja/crabstick/LILJA_BCK/computer_files/.* /media/lilja/crabstick/LILJA_BCK/computer_files/temp
 
-cp -rf ~/Desktop ~/Documents ~/drawings ~/executables ~/games ~/Music ~/programs ~/Videos ~/.minetest ~/.secret /media/lilja/crabstick/LILJA_BCK/computer_files
+echo "Copying"
+cp -rf ~/Desktop ~/Documents ~/drawings ~/executables ~/games ~/Music ~/programs ~/Videos ~/robotics ~/.minetest ~/.secret /media/lilja/crabstick/LILJA_BCK/computer_files
+
+echo "Removing temp"
+rm -rf temp/
