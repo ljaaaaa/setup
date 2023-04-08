5aee64c9fefb032d1509e1d80bcc97c57ee8aec0
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2023-04-07 18:58:50 -0700

    wooish

--- install.sh
+++ install.sh
@@ -53,6 +53,8 @@ echo SPOTIFY; snap install spotify
 echo GIMP; apt install gimp
 echo LOCATE; apt install mlocate
 echo CLOCKS; apt install gnome-clocks
+
+#Music works now
 echo SUS; apt install ubuntu-restricted-extras
 echo SUS2; apt remove gstreamer1.0-vaapi
 # Chrome (online download)
