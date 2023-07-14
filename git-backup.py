128a58bc7ed3c27873ba78db080d7972c37b2faa
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2023-07-13 20:17:22 -0700

    git backups update

--- git-backup.py
+++ git-backup.py
@@ -1,22 +1,22 @@
-#!/bin/python3
+# !/bin/python3
+# Move this file into the directory you want backed up repositories in
 
 import os
 import requests
 
 # Method that returns list of all Lilja's repositories
-def get_lilja_repos()
+def get_lilja_repos():
     url = 'https://api.github.com/users/LiljaKiiski/repos'    
 
     try:
+        response = requests.get(url)
         response.raise_for_status()
 
         repositories = response.json()
-
         my_repos = []
 
         for repo in repositories:
-            print(repo['name'])
-            my_repos.append(repo['name'])
+            my_repos.append('LiljaKiiski/' + repo['name'])
 
         # Return list
         return my_repos
@@ -36,8 +36,15 @@ repos.append('mvrt-115/frc2k23')
 # Create a directory to back these up to
 os.system('mkdir ~/git-backups')
 
+print('GitHub repositories backup START')
+
 # Clone those repositories
 for repo in repos:
+    print('')
+    print('')
     print('Cloning ' + repo + '...')
     os.system('git clone git@github.com:' + repo)
-    os.system('mv ' + repo + ' ~/git-backups')
+
+print('')
+print('')
+print('GitHub repositories backup END')
